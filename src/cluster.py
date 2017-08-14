911aebe42eb276a5e958c365c0b6b921d4a3b7ba
Arsh Khandelwal <khandelwal.arsh@gmail.com>
2017-08-13 22:34:50 -0700

    directory restructuring

---
+++ src/cluster.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+import pandas as pd
+import nltk
+import re
+import os
+import codecs
+from sklearn import feature_extraction
+from sklearn.cluster import Birch
+from sklearn.decomposition import PCA
+import mpld3
+from nlp import tokenize_and_stem
+from nlp import tokenize_only
+
+
+def cluster(df, reduction=False):
+        """
+        df = sp_matrix
+        reduction -> pca dimensional reduction
+        """
+        if reduction is True:
+            df = df.toarray()
+            pca = PCA().fit(df)
+            X = pca.transform(df)
+        else:
+            X = df
+        threshold = 1.5
+        # eventually standardize parameter list as function argument and allow for different algorithms to be used
+        model = Birch(threshold=threshold, n_clusters=None)
+        out = model.fit_predict(X)
+        labels = model.labels_
+        subcluster_labels = model.subcluster_labels_
+        centroids = model.subcluster_centers_
+        # model = Birch
+        # centroids = cluster centroids
+        # out = set of labels
+        return out, model, centroids
+
+
+def visualize(tweets, labels, centroids):
+    print("visualize")
+
+
+def print_cluster(tweets, labels):
+    for idx, val in enumerate(tweets):
+        print(labels[idx])
+        print(val['tweet'])
+
+
+def vectorize(tweets):
+    """ function to generate features and then vectorize into sparse matrix """
+    dv = feature_extraction.DictVectorizer()
+    fv = []
+    for tweet in tweets:
+        features = {}
+        for word in tweet['tokenized_tweet']:
+            features[word] = 1
+        fv.append(features)
+    sp_fv = dv.fit_transform(fv)
+    return sp_fv
+
+
+def main():
+    # read csv into df
+    df = pd.read_csv('../data/user_data/ArshKhandelwal_tweets.csv')
+    tweets = df.to_dict('records')
+    for tweet in tweets:
+        tweet['tokenized_tweet'] = tokenize_only(tweet['tweet'])
+        tweet['tokenize_stemmed_tweet'] = tokenize_and_stem(tweet['tweet'])
+    sp_fv = vectorize(tweets)
+    # 1 cluster tweets based on word frequency after dropping stop words
+    labels, model, centroids = cluster(sp_fv)
+
+    # 2 NLP Sentiment analysis
+    # 3 combine all hashtags from all tweets and determine overall sentiment of user
+
+
+if __name__ == '__main__':
+    main()
--- src/nlp.py
+++ src/nlp.py
@@ -7,53 +7,10 @@ import os
 import codecs
 from sklearn import feature_extraction
 from sklearn.cluster import Birch
+from sklearn.decomposition import PCA
 import mpld3
 from nltk.stem.snowball import SnowballStemmer
 
-
-def cluster(df, reduction=False):
-        """
-        df = sp_matrix
-        reduction -> pca dimensional reduction
-        """
-        if reduction is True:
-            df = df.toarray()
-            pca = PCA().fit(df)
-            X = pca.transform(df)
-        else:
-            X = df
-
-        threshold = 1.5
-
-        # eventually standardize parameter list as function argument and allow for different algorithms to be used
-        model = Birch(threshold=threshold, n_clusters=None)
-        out = model.fit_predict(X)
-
-        labels = model.labels_
-        subcluster_labels = model.subcluster_labels_
-        centroids = model.subcluster_centers_
-
-        # model = Birch
-        # centroids = cluster centroids
-        # out = set of labels
-        return out, model, centroids
-
-def print_cluster(tweets, labels):
-    for idx, val in enumerate(tweets):
-        print(labels[idx])
-        print(val['tweet'])
-
-def vectorize(tweets):
-    """ function to generate features and then vectorize into sparse matrix """
-    dv = feature_extraction.DictVectorizer()
-    fv = []
-    for tweet in tweets:
-        features = {}
-        for word in tweet['tokenized_tweet']:
-            features[word] = 1
-        fv.append(features)
-    sp_fv = dv.fit_transform(fv)
-    return sp_fv
 
 def tokenize_and_stem(text):
     stemmer = SnowballStemmer("english")
@@ -77,28 +34,3 @@ def tokenize_only(text):
         if re.search('[a-zA-Z]', token):
             filtered_tokens.append(token)
     return filtered_tokens
-
-
-def main():
-    # read csv into df
-    df = pd.read_csv('../data/user_data/ArshKhandelwal_tweets.csv')
-    tweets = df.to_dict('records')
-
-    # ['time', 'tweet', 'id', 'retweet_count', 'favorite_count', 'lang']
-    for tweet in tweets:
-        tweet['tokenized_tweet'] = tokenize_only(tweet['tweet'])
-        tweet['tokenize_stemmed_tweet'] = tokenize_and_stem(tweet['tweet'])
-
-    sp_fv = vectorize(tweets)
-    labels, model, centroids = cluster(sp_fv)
-
-    # TODO
-    # parse csv tweets one at a time
-    # potential outputs
-        # 1 cluster tweets based on word frequency after dropping stop words DONE
-        # 2 NLP Sentiment analysis
-        # 3 combine all hashtags from all tweets and determine overall sentiment of user
-
-
-if __name__ == '__main__':
-    main()
